b2a5e399696c0d0aec60070af0c5e870fc7fd188
Tanishqa <tanishqadivyam@gmail.com>
2022-02-10 12:40:28 +0530

    Added Index and Nav

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>homepage</title>
+</head>
+<body>
+    <?php include 'nav.php'; ?>
+    <div class="w3-display-container" style="background-image:linear-gradient(pink,#E0FFF5); min-height: 80vh;"> 
+        <div class="w3-card-4 w3-display-middle" style="background-image:linear-gradient(#7AFF02,green);padding: 5rem; border-radius:3.8rem">
+            <h1 style="font-family:cursive; font-size:7rem;"><b>QuizIt</b></h1>
+            <h2 style="font-family:cursive;"><i>A site to test your knowledge!</i></h2>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,5 @@
+<div class="w3-bar" style="background-image: linear-gradient(#004E0F,black);">
+<b>
+    <a href="#" class="w3-bar-item w3-btn w3-hover-green" style="font-family:cursive;font-size:25px;color:white;">Home</a><!-- w3 ke bar ka item-->
+</b>
+</div>
